39f66e63cf82ad9c697486e5d16730cd9b814cc5
Jon Austen <djangofan@gmail.com>
2023-12-02 15:41:14 -0800

    Add title component for image and title phrase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home.component';
 import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    HeaderComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
