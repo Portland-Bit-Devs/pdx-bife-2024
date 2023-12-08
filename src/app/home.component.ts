1db2e441f550c60d1b666632e4df0fa561cab548
Jon Austen <djangofan@gmail.com>
2023-12-07 18:39:39 -0800

    Switched theme to black with orange fonts.

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  messages: string[] = ["Welcome!", "Latest News", "Important Update"];
+  messages: string[] = ["Welcome!", "Latest News"];
 
   constructor() { }
 
