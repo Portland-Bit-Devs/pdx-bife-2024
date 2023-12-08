b7c5ec9bd7747e67443a43feb6fc56a9671c1edd
Jon Austen <djangofan@gmail.com>
2023-12-07 16:45:05 -0800

    Add news popups on home page.

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  messages: string[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+     this.messages = ["Welcome!", "Latest News", "Important Update"];
+  }
+
+  closePopup(message: string) {
+    this.messages = this.messages.filter(m => m !== message);
+  }
 
 }
